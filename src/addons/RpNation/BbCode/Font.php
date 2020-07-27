d88dafeca3bcc666a4e7e97d8972e1909f9e3f69
Alteras1 <42795314+Alteras1@users.noreply.github.com>
2020-07-27 14:44:59 -0700

    Add font weight options to gfont
    added multiple argument support for gfont.

--- src/addons/RpNation/BbCode/Font.php
+++ src/addons/RpNation/BbCode/Font.php
@@ -10,27 +10,63 @@ class Font
 		'arial', 'book antiqua', 'courier new', 'georgia', 'tahoma', 'times new roman', 'trebuchet ms', 'verdana'
 	);
 
+	private const STYLES = array(
+		'thin', 'extralight', 'light', 'regular', 'medium', 'semibold', 'bold', 'extrabold', 'black', 'italic'
+	);
+
+	private const WEIGHTS = array(
+		'100', '200', '300', '400', '500', '600', '700', '800', '900','i'
+	);
+
 	public static function renderFontTag($tagChildren, $tagOption, $tag, array $options, AbstractRenderer $renderer)
 	{
 		$text = $renderer->renderSubTree($tagChildren, $options);
-		$font = strtolower(trim($tagOption));
-
+		$font = null;
+		$style = null;
+		$fontweight = '400';
+		$fontstyle = 'normal';
+		
+		if (is_array($tagOption))
+		{
+			$font = $tagOption['name'] ?? $tagOption['family'] ?? "";
+			$style = $tagOption['style'] ?? null;
+		}
+		else
+		{
+			$font = $tagOption;
+		}
+		
 		if (empty(trim($text)) || empty($font))
 		{
 			return $text;
 		}
+		$font = strtolower(trim($font));
 
 		if (in_array($font, self::FONTS))
 		{
 			return "<span style=\"font-family: '$font'\">" . $text . "</span>";
 		}
 
-		$font = htmlspecialchars(addslashes(trim($tagOption)));
+		$font = htmlspecialchars(addslashes($font));
 		if (is_subclass_of($renderer, 'XF\BbCode\Renderer\Html'))
 		{
 			$webfont = str_replace(' ', '+', $font);
+			if ($style != null)
+			{
+				$style = str_replace(self::STYLES, self::WEIGHTS, str_replace([' ', '-'], '', strtolower(trim($style))));
+				$webfont .= ':' . $style;
+				if (stripos($style, 'i') !== false)
+				{
+					$fontstyle = "italic";
+					$style = str_replace('i', '', $style);
+				}
+				if (strlen($style) !== 0)
+				{
+					$fontweight = trim($style);
+				}
+			}
 			$renderer->getTemplater()->inlineJs("loadWebfont('$webfont');");
 		}
-		return "<span style=\"font-family: '$font';\">$text</span>";
+		return "<span style=\"font-family: '$font'; font-style: '$fontstyle'; font-weight: '$fontweight';\">$text</span>";
 	}
 }
