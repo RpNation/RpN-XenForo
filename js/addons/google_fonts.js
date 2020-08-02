e023e8a35108bc72f95cced77578e3efc4080e3a
MShultz <mary.shultz.2018@gmail.com>
2020-08-02 13:57:57 -0600

    Removes CSS2 Annotation from JS API call

--- js/addons/google_fonts.js
+++ js/addons/google_fonts.js
@@ -1,11 +1,11 @@
 var webfonts = [];
 
 function loadWebfont(font) {
-	if(!($.inArray(font, webfonts) + 1)) {
-		$('head').append(
-			"<link href='https://fonts.googleapis.com/css2?family=" + font + "' rel='stylesheet' type='text/css'>"
-		);
-		webfonts.push(font);
-		console.log('Loaded Font: ' + font);
-	}
+  if (!($.inArray(font, webfonts) + 1)) {
+    $("head").append(
+      "<link href='https://fonts.googleapis.com/css?family=" + font + "' rel='stylesheet' type='text/css'>"
+    );
+    webfonts.push(font);
+    console.log("Loaded Font: " + font);
+  }
 }
