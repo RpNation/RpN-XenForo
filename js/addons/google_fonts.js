ae3bd4e92e94ed2039435ce0ec39c8b7841e840e
MShultz <mary.shultz.2018@gmail.com>
2020-08-02 13:59:55 -0600

    Reverts prettier changes for tickmark preference.

--- js/addons/google_fonts.js
+++ js/addons/google_fonts.js
@@ -2,10 +2,10 @@ var webfonts = [];
 
 function loadWebfont(font) {
   if (!($.inArray(font, webfonts) + 1)) {
-    $("head").append(
+    $('head').append(
       "<link href='https://fonts.googleapis.com/css?family=" + font + "' rel='stylesheet' type='text/css'>"
     );
     webfonts.push(font);
-    console.log("Loaded Font: " + font);
+    console.log('Loaded Font: ' + font);
   }
 }
