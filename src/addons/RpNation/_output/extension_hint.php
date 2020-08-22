f84118f28180d5f4160af9a040870aaeda3ecfae
Mondrethos <awizarddarkly@gmail.com>
2020-08-21 22:34:11 -0400

    Adding Other BBCode
    Added the non php bbcodes to the repo/addon.

---
+++ src/addons/RpNation/_output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace RpNation\BbCode
+{
+	class XFCP_RuleSet extends \XF\BbCode\RuleSet {}
+}
